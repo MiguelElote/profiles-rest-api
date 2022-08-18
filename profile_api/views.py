0054434306520517c46af53521012c0a9c054770
Michael Cornn <michael.cornn@gmail.com>
2022-08-17 20:27:51 -0400

    Updated login API

--- profile_api/views.py
+++ profile_api/views.py
@@ -7,6 +7,8 @@ from rest_framework import filters
 from profile_api import serializers
 from profile_api import models
 from profile_api import permissions
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings
 
 
 class HelloAPIView(APIView):
@@ -100,3 +102,7 @@ class UserProfileViewSet(viewsets.ModelViewSet):
     permission_classes = (permissions.UpdateOwnProfile,)
     filter_backends = (filters.SearchFilter,)
     search_fields = ('name','email',)
+
+class UserLoginApiView(ObtainAuthToken):
+    """Handle creating a user authentication token"""
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
